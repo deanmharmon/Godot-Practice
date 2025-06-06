7abd922095755690cc01485cf36f59bc90c07445
Dean Mason <dean.hj.mason@gmail.com>
2025-06-06 16:14:15 -0400

    Movement working

---
+++ Scripts/Player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+
+@export var speed = 250
+
+func _ready():
+	print("Running!")
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	print("Player pos:", global_position)
+	print("Sprite pos:", $AnimatedSprite2D.global_position)
+		
+		
+	
